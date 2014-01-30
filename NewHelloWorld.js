3c90e506840ac94f29b773adb452f5d20b0bd784
agreenturtle <james.lai.chen@gmail.com>
2014-01-30 13:22:54 -0800

    Updated NewHelloWorld
    Just adding a few lines of code

--- NewHelloWorld.js
+++ NewHelloWorld.js
@@ -2,6 +2,12 @@
 
 function HelloWorld(){
     console.log("Hello World!!!");
+    console.log("adding a few lines of code");
+    var condition = true;
+    if (condition)
+        console.log("true");
+    else
+        console.log("false");
 }
 
 HelloWorld();
