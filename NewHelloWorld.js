6981b6d08cae4f80820bc039fe7527fcb3be84da
agreenturtle <james.lai.chen@gmail.com>
2014-01-30 13:58:39 -0800

    delete lines
    deleted some lines

--- NewHelloWorld.js
+++ NewHelloWorld.js
@@ -4,10 +4,7 @@ function HelloWorld(){
     console.log("Hello World!!!");
     console.log("adding a few lines of code");
     var condition = true;
-    if (condition)
-        console.log("true");
-    else
-        console.log("false");
+
 }
 
 HelloWorld();
